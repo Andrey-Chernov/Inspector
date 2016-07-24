e2781594cfdcdaa145b455c287baad0f0f330e02
Andrey <Andrey.Chernov@list.ru>
2016-07-24 15:53:41 +0300

    Разработка начата

---
+++ src/ru/ach/javasource/inspector/SrcInspector.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ru.ach.javasource.inspector;
+
+/**
+ * @author Андрей
+ *
+ */
+public class SrcInspector {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
